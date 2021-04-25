ba94e2d7b470fbcc3e0aeb3cd1514f0a21e38573
yombi <ntwariegide2@gmail.com>
2021-04-25 21:50:40 +0200

    connecting welcome page

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-welcome-page',
+  selector: 'welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.css']
 })
