772c271b70438a9a55110a4d618be9a00e0c9354
yombi <ntwariegide2@gmail.com>
2021-04-25 20:12:26 +0200

    added welcome page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeNavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/home-navbar/home-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home-navbar',
+  templateUrl: './home-navbar.component.html',
+  styleUrls: ['./home-navbar.component.css']
+})
+export class HomeNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
