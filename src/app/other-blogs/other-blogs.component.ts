6a8abbf827882de341ca88bfedbc191cf94b7c3a
yombi <ntwariegide2@gmail.com>
2021-04-26 22:57:19 +0200

    added the list of looping in other blogs

--- src/app/other-blogs/other-blogs.component.ts
+++ src/app/other-blogs/other-blogs.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OtherBlogsComponent implements OnInit {
 
+  otherBlogsList = [1,2,3,4,5,6,7,8,9]
+
   constructor() { }
 
   ngOnInit(): void {
