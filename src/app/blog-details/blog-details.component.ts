1daa437e0afb4947fb84f8b682261fb5104816b0
yombi <ntwariegide2@gmail.com>
2021-04-27 13:59:07 +0200

    added first paragraph

--- src/app/blog-details/blog-details.component.ts
+++ src/app/blog-details/blog-details.component.ts
@@ -9,6 +9,10 @@ export class BlogDetailsComponent implements OnInit {
 
   constructor() { }
 
+  firstParagraph = "Most, if not all, careers involve some computer-based work and require strong typing skills. Information technology professionals, for example, must be efficient typists in order to write code for computer and software programs. Accuracy is equally as important as speed, as a simple typo could result in a system-wide error"
+
+ secondParagraphy = "In today’s technology-driven world, students and young adults become familiar with computers at an early age. Some schools have stopped teaching typing altogether, assuming students already possess the skills necessary to be successful in the modern workplace, according to an article in MIT’s Technology Review."
+
   ngOnInit(): void {
   }
 
