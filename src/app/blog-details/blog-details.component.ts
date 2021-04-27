2b2cc0ae0e92c6ca48b6d2c00588dc19fcdc258c
yombi <ntwariegide2@gmail.com>
2021-04-27 09:25:17 +0200

    linking blog-details

--- src/app/blog-details/blog-details.component.ts
+++ src/app/blog-details/blog-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-blog-details',
+  selector: 'blog-details',
   templateUrl: './blog-details.component.html',
   styleUrls: ['./blog-details.component.css']
 })
