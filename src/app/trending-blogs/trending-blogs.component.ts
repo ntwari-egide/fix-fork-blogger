a33a252a7dc612485b30ace79e91aa8902289463
yombi <ntwariegide2@gmail.com>
2021-04-28 09:54:49 +0200

    Adding dynamic data

--- src/app/trending-blogs/trending-blogs.component.ts
+++ src/app/trending-blogs/trending-blogs.component.ts
@@ -8,8 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrendingBlogsComponent implements OnInit {
   listOfTrendingBlogs = [{
-    coverPic : 'https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309__340.jpg'
-  },2,3,4,5,6]
+      coverPic : 'https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309__340.jpg',
+      title: 'Advanced and Applied AI on Microsoft Azure',
+      description: 'Enhance your understanding of machine learning and AI using Microsoft Azure and Python.',
+      content: '',
+      likes: 11,
+      forks: 9,
+      comments: 3
+  },{
+    coverPic : 'https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309__340.jpg',
+    title: 'Advanced and Applied AI on Microsoft Azure',
+    description: 'Enhance your understanding of machine learning and AI using Microsoft Azure and Python.',
+    content: '',
+    likes: 11,
+    forks: 9,
+    comments: 3  
+  },
+  {
+    coverPic : 'https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309__340.jpg',
+    title: 'Advanced and Applied AI on Microsoft Azure',
+    description: 'Enhance your understanding of machine learning and AI using Microsoft Azure and Python.',
+    content: '',
+    likes: 11,
+    forks: 9,
+    comments: 3 
+  },
+  {
+    coverPic : 'https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309__340.jpg',
+    title: 'Advanced and Applied AI on Microsoft Azure',
+    description: 'Enhance your understanding of machine learning and AI using Microsoft Azure and Python.',
+    content: '',
+    likes: 11,
+    forks: 9,
+    comments: 3
+  },
+  {
+    coverPic : 'https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309__340.jpg',
+    title: 'Advanced and Applied AI on Microsoft Azure',
+    description: 'Enhance your understanding of machine learning and AI using Microsoft Azure and Python.',
+    content: '',
+    likes: 11,
+    forks: 9,
+    comments: 3
+  },{
+    coverPic : 'https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309__340.jpg',
+    title: 'Advanced and Applied AI on Microsoft Azure',
+    description: 'Enhance your understanding of machine learning and AI using Microsoft Azure and Python.',
+    content: '',
+    likes: 11,
+    forks: 9,
+    comments: 3
+  }]
   
   constructor() { }
 
