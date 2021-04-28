87643840fdb1694af3f27282f5ea3f5ca4703ed8
yombi <ntwariegide2@gmail.com>
2021-04-28 11:15:35 +0200

    passing parameter to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,6 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { BlogDetailsComponent } from './blog-details/blog-details.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import { IndexComponent } from './components/index/index.component'
-// import { AuthComponent } from './components/auth/auth.component'
-// import { SuperAdminComponent } from './components/superAdmin/super-admin/super-admin.component'
-// import { UserDaashboardComponent } from './components/user-daashboard/user-daashboard.component'
-// import { SellerDarshboardComponent } from './components/seller-darshboard/seller-darshboard.component'
-// import { ExproleProductComponent } from './components/exprole-product/exprole-product.component'
-// import { ListProductsComponent } from './components/list-products/list-products.component'
-// import { CustomerAccountComponent } from './components/customer-account/customer-account.component'
-// import { VendorApplicationComponent} from './components/vendor-application/vendor-application.component'
-
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'index'},
   { path: 'read-blog', component : BlogDetailsComponent},
