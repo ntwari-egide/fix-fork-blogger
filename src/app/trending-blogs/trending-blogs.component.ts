bab03fa98dc204c134140b0bd8bf131a1d55ec29
yombi <ntwariegide2@gmail.com>
2021-04-28 10:08:03 +0200

    adding dynamic data

--- src/app/trending-blogs/trending-blogs.component.ts
+++ src/app/trending-blogs/trending-blogs.component.ts
@@ -27,37 +27,20 @@ export class TrendingBlogsComponent implements OnInit {
   {
     coverPic : 'https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501__340.jpg',
     title: 'IT & Computer Science Courses',
-    description: 'Enhance your understanding of machine learning and AI using Microsoft Azure and Python.',
+    description: 'From learning to code and exploring programming to understanding cyber security, take online IT and coding courses to explore tech trends like data science and digital marketing.',
     content: '',
-    likes: 11,
-    forks: 9,
-    comments: 3 
+    likes: 211,
+    forks: 1,
+    comments: 12 
   },
   {
-    coverPic : 'https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309__340.jpg',
-    title: 'Advanced and Applied AI on Microsoft Azure',
-    description: 'Enhance your understanding of machine learning and AI using Microsoft Azure and Python.',
+    coverPic : 'https://cdn.pixabay.com/photo/2015/10/29/08/17/building-1011876__340.jpg',
+    title: 'AI Design and Engineering with Microsoft Azure',
+    description: 'Develop AI Engineering skills using Microsoft Azure, on the path to role-based certifications.',
     content: '',
-    likes: 11,
-    forks: 9,
-    comments: 3
-  },
-  {
-    coverPic : 'https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309__340.jpg',
-    title: 'Advanced and Applied AI on Microsoft Azure',
-    description: 'Enhance your understanding of machine learning and AI using Microsoft Azure and Python.',
-    content: '',
-    likes: 11,
-    forks: 9,
-    comments: 3
-  },{
-    coverPic : 'https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309__340.jpg',
-    title: 'Advanced and Applied AI on Microsoft Azure',
-    description: 'Enhance your understanding of machine learning and AI using Microsoft Azure and Python.',
-    content: '',
-    likes: 11,
-    forks: 9,
-    comments: 3
+    likes: 32,
+    forks: 0,
+    comments: 13
   }]
   
   constructor() { }
