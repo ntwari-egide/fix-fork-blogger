31bd84a08b46b93fe1ac3b214aa2381ff9891e59
yombi <ntwariegide2@gmail.com>
2021-04-30 13:54:53 +0200

    updating the sliders

--- src/app/trending-blogs/trending-blogs.component.ts
+++ src/app/trending-blogs/trending-blogs.component.ts
@@ -43,52 +43,9 @@ export class TrendingBlogsComponent implements OnInit {
     comments: 13
   }]
   
-  imgags = [
-    'https://cdn.pixabay.com/photo/2016/02/19/11/23/women-1209678__340.jpg',
-    'https://cdn.pixabay.com/photo/2016/02/19/11/23/women-1209678__340.jpg',
-    'https://cdn.pixabay.com/photo/2016/02/19/11/23/women-1209678__340.jpg',
-    'https://cdn.pixabay.com/photo/2016/02/19/11/23/women-1209678__340.jpg'
-  ];
-  public carouselTileItems: Array<any> = [0, 1, 2, 3, 4, 5];
-  public carouselTiles = {
-    0: [],
-    1: [],
-    2: [],
-    3: [],
-    4: [],
-    5: []
-  };
-  public carouselTile: NguCarouselConfig = {
-    grid: { xs: 1, sm: 1, md: 3, lg: 3, xl:3, all: 0 },
-    slide: 3,
-    speed: 250,
-    point: {
-      visible: true
-    },
-    load: 2,
-    velocity: 0,
-    touch: true,
-    easing: 'cubic-bezier(0, 0, 0.2, 1)'
-  };
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {
-    this.carouselTileItems.forEach(el => {
-      this.carouselTileLoad(el);
-    });
+  ngOnInit(): void {
   }
-
-  public carouselTileLoad(j) {
-    // console.log(this.carouselTiles[j]);
-    const len = this.carouselTiles[j].length;
-    if (len <= 30) {
-      for (let i = len; i < len + 15; i++) {
-        this.carouselTiles[j].push(
-          this.imgags[Math.floor(Math.random() * this.imgags.length)]
-        );
-      }
-    }
-  }
-}
 
 }
